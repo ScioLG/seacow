812edc5ace9727fa4d86dbf09f96fa3a4d5a0bfd
ScioLG <92563965+ScioLG@users.noreply.github.com>
2023-07-11 21:02:42 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import telebot
+from telebot import types
+bot = telebot.TeleBot('6143255807:AAGHDgHlVlhrLMvWIHRMgS7qtPV1YqbBRM4')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    but1 = types.KeyboardButton('амогус')
+    but2 = types.KeyboardButton('абобус')
+    but3 = types.KeyboardButton('глобус')
+    markup.add(but1, but2, but3)
+
+    bot.send_message(message.chat.id, 'здарова', reply_markup=markup)
+
+
+bot.polling(none_stop=True)
